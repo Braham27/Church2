70c35dc8094e52442c484893d6f544d2bd507993
Abraham Cadet <abrahamcadet10@gmail.com>
2019-03-09 02:43:46 -0500

    Update admin_header.php

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -17,14 +17,23 @@ if (!isset($_SESSION['position'])) {
     <title>NSEMC</title>
     <meta name="description" content="A high-quality &amp; free Bootstrap admin dashboard template pack that comes with lots of templates and components.">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
     <link rel="stylesheet" href="styles/css.css">
+
     <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js">
+
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    
     <link rel="stylesheet" id="main-stylesheet" data-version="1.1.0" href="styles/a.c.1.1.0.min.css">
+    
     <link rel="stylesheet" href="styles/ac.1.1.0.min.css">
+    
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/quill/1.3.6/quill.snow.css"> 
+    
     <script async defer src="https://buttons.github.io/buttons.js"></script>
 
     <script>
