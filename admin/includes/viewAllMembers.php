c1a769b04d06acc6f9d4ff5b0a2af731e75bb7c2
Abraham Cadet <abrahamcadet10@gmail.com>
2019-03-11 14:16:03 -0400

    Update viewAllMembers.php

--- admin/includes/viewAllMembers.php
+++ admin/includes/viewAllMembers.php
@@ -92,7 +92,14 @@
 
 <!-- data-toggle="modal" data-target=".bd-example-modal-lg"  -->
 <!-- rel="<?php  $user_id?>" -->
-
+                        <input type="hidden" class="" value="">
+                        <input type="hidden" class="" value="">
+                        <input type="hidden" class="" value="">
+                        <input type="hidden" class="" value="">
+                        <input type="hidden" class="" value="">
+                        <input type="hidden" class="" value="">
+                        <input type="hidden" class="" value="">
+                        
                         <a class="pl-1 myLink pr-2 py-0"  id="popover" data-trigger="hover" rel="<?php echo $user_tel?>" 
                         data-content="<?php echo $user_first ?>" value="<?php echo $user_last . " " . $user_first ?>" data-toggle="modal" data-target=".bd-example-modal-lg"
                         title="Send A sms to <?php echo $user_first ?>" href='javascript:void(0)' data-toggle="popover" data-placement="top">
@@ -111,53 +118,11 @@ $(".myLink").click(function(){
   $('input.phone').val(phone);
 
       });
-
-
 </script>           
 
-<div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-hidden="true">
-  <div class="modal-dialog modal-lg">
-    <div class="modal-content">
-      <!-- /BEGINING OF SMS -->
-                  
-  <div class="container-contact100">
 
-    <span class="contact100-form-title mb-4">
-      Send A Text
-    </span>
-
-    <div class="wrap-input100 rs1-wrap-input100 validate-input" data-validate="Name is required">
-      <span class="label-input100">Member's Name</span>
-      <input class="input100 fl" type="text" name="name" value="">
-      <span class="focus-input100"></span>
-    </div>
-
-    <div class="wrap-input100 rs1-wrap-input100 validate-input" data-validate = "Phone is required: 12345678">
-      <span class="label-input100">Phone</span>
-      <input class="input100 phone" type="tel" name="email" value="">
-      <span class="focus-input100"></span>
-    </div>
-
-    <div class="wrap-input100 validate-input" data-validate = "Message is required">
-      <span class="label-input100">Message</span>
-      <textarea class="input100" name="message" placeholder="Your message here..."></textarea>
-      <span class="focus-input100"></span>
-    </div>
-
-    <div class="container-contact100-form-btn">
-      <button class="contact100-form-btn">
-        <span>
-          Submit
-          <i class="fa fa-long-arrow-right m-l-7" aria-hidden="true"></i>
-        </span>
-      </button>
-    </div>
-  </form>
-
-</div>
-</div>
-<div id="dropDownSelect1"></div>
-
+<!-- /BEGINING OF SMS -->
+      <?php include "sms.php"; ?>
 <!-- /END OF SMS -->
                                   
                             </div>
