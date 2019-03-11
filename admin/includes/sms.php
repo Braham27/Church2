bda0e294a04ff964146d1cc79a09ae5060360022
Abraham Cadet <abrahamcadet10@gmail.com>
2019-03-11 14:16:00 -0400

    Create sms.php

---
+++ admin/includes/sms.php
@@ -0,0 +1,40 @@
+<div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+        <div class="container-contact100">
+
+<span class="contact100-form-title mb-4">
+  Send A Text
+</span>
+
+<div class="wrap-input100 rs1-wrap-input100 validate-input" data-validate="Name is required">
+  <span class="label-input100">Member's Name</span>
+  <input class="input100 fl" type="text" name="name" value="">
+  <span class="focus-input100"></span>
+</div>
+
+<div class="wrap-input100 rs1-wrap-input100 validate-input" data-validate = "Phone is required: 12345678">
+  <span class="label-input100">Phone</span>
+  <input class="input100 phone" type="tel" name="email" value="">
+  <span class="focus-input100"></span>
+</div>
+
+<div class="wrap-input100 validate-input" data-validate = "Message is required">
+  <span class="label-input100">Message</span>
+  <textarea class="input100" name="message" placeholder="Your message here..."></textarea>
+  <span class="focus-input100"></span>
+</div>
+
+<div class="container-contact100-form-btn">
+  <button class="contact100-form-btn">
+    <span>
+      Submit
+      <i class="fa fa-long-arrow-right m-l-7" aria-hidden="true"></i>
+    </span>
+  </button>
+</div>
+</form>
+
+</div>
+</div>
+<div id="dropDownSelect1"></div>
