45d16e6d067ae7dfcd9fe5757beca6d892982893
Abraham Cadet <abrahamcadet10@gmail.com>
2019-04-19 20:09:02 -0400

    Update script.js

--- admin/scripts/script.js
+++ admin/scripts/script.js
@@ -1,9 +1,9 @@
 
-    function myFunction() {
-        const filter = document.querySelector('#myInput').value.toUpperCase();
-        const trs = document.querySelectorAll('#myTable tr:not(.header)');
-        trs.forEach(tr => tr.style.display = [...tr.children].find(td => td.innerHTML.toUpperCase().includes(filter)) ? '' : 'none');
-      }
+    // function myFunction() {
+    //     const filter = document.querySelector('#myInput').value.toUpperCase();
+    //     const trs = document.querySelectorAll('#myTable tr:not(.header)');
+    //     trs.forEach(tr => tr.style.display = [...tr.children].find(td => td.innerHTML.toUpperCase().includes(filter)) ? '' : 'none');
+    //   }
 
       $(function () {
         $('#popover').popover();
