76d52fe9e2a075f7f6c98460a08f8ef27c219328
Abraham Cadet <abrahamcadet10@gmail.com>
2019-04-20 23:08:33 -0400

    Update user-profile-lite.php

--- admin/user-profile-lite.php
+++ admin/user-profile-lite.php
@@ -102,9 +102,10 @@ $query = "SELECT * FROM users WHERE user_email = '{$email}'";
                               <div class="form-group col-md-6">
                                 <label for="feEmailAddress">Email</label>
                                 <input type="email" class="form-control" name="email" id="feEmailAddress" placeholder="Email" value="paul@example.com"> </div>
-                              <div class="form-group col-md-6">
+                              <!-- <div class="form-group col-md-6">
                                 <label for="fePassword">Password</label>
-                                <input type="password" class="form-control" name="pass" id="fePassword" placeholder="Password"  value="<?php ?>"> </div>
+                                <input type="password" class="form-control" name="pass" id="fePassword" placeholder="Password"  value="<?php  ?>"> </div> -->
+                                <input id="spass" type="checkbox"> <p id="spasst">Change the Password </p>
                             </div>
                             <div class="form-group">
                               <label for="feInputAddress">Address</label>
