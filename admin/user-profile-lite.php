64bf2ffa34a01dcd8041542c25be0408533b0c10
Abraham Cadet <abrahamcadet10@gmail.com>
2019-04-22 13:49:26 -0400

    Update user-profile-lite.php

--- admin/user-profile-lite.php
+++ admin/user-profile-lite.php
@@ -20,13 +20,7 @@
 ?>
           </div> 
           <!-- ALERT -->
-          <div class="alert alert-success alert-dismissible fade show mb-0" role="alert">
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-              <span aria-hidden="true">×</span>
-            </button>
-            <i class="fa fa-check mx-2"></i>
-            <strong>Success!</strong> Your profile has been updated! 
-          </div>
+         
           <!-- END ALERT -->
 
           <div class="main-content-container container-fluid px-4">
